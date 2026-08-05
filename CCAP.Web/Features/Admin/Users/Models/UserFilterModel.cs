071fae9ddd4fbc2fb94d531824ebfb3ae817f859
Paulo <pauloguevarra10@gmail.com>
2026-08-05 10:14:05 -0700

    Admin UI Update

---
+++ CCAP.Web/Features/Admin/Users/Models/UserFilterModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CCAP.Web.Features.Admin.Users.Models
+{
+
+    public class UserFilterModel
+    {
+        public string Search { get; set; } = string.Empty;
+
+        public Guid? RoleId { get; set; }
+
+        public Guid? DisciplineId { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}
